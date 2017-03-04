93dc8a5027bb421dab8e9ec73999687b040236ad
ottyajp <ottyajp@gmail.com>
2017-03-04 13:55:23 +0900

    ダウンロードしたりディストリチェックしたりするの追加

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+cd $HOME
 PWD=`pwd`
+
+echo -n checking distribution... 
+export dist_name
+if [ -e /etc/arch-release ]; then
+  dist_name="arch"
+fi
+echo $dist_name
+
+echo -n checking git... 
+git --version >/dev/null
+if [ $? -ne 0 ]; then
+  [[ "$dist_name" == "arch" ]] && sudo pacman -S git
+fi
+
+echo downloading...
+git clone --recursive https://github.com/ottyajp/dotfiles.git ./dotfiles
 
 echo creating symbolic links
 for f in .??*;do
