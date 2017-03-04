52c76fc7cf315eba2bc4066bc9f93e09720a5374
ottyajp <ottyajp@gmail.com>
2017-03-04 14:11:57 +0900

    ユーザ名違ってた時に死ぬの修正

--- .zsh/conf.d/10-base.zsh
+++ .zsh/conf.d/10-base.zsh
@@ -17,7 +17,7 @@ bindkey -v
 #zstyle :compinstall filename '/home/tea/.zshrc'
 
 # completion
-eval $(dircolors /home/tea/.zsh/dircolors-solarized/dircolors.ansi-universal)
+eval $(dircolors $HOME/.zsh/dircolors-solarized/dircolors.ansi-universal)
 autoload -U compinit
 compinit
 setopt correct
@@ -25,7 +25,7 @@ zstyle ':completion:*:default' menu select=2
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
 # syntax highlighting
-source /home/tea/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # command not found
 [ -r /etc/profile.d/cnf.sh ] && . /etc/profile.d/cnf.sh
