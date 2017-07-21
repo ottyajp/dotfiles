7f32fbc4a87e9cd47d60c1d216e89dc3806276af
ottyajp <ottyajp@gmail.com>
2017-07-21 11:17:10 +0900

    gitのステータスをPROMPTに表示するように

--- .zsh/conf.d/30-prompt.zsh
+++ .zsh/conf.d/30-prompt.zsh
@@ -1,9 +1,19 @@
+# git
+autoload -Uz vcs_info
+setopt prompt_subst
+zstyle ':vcs_info:git:*' check-for-changes true
+zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"     # %u
+zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"    # %c
+zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"  # $vcs_info_msg_0_, %b:branch name
+zstyle ':vcs_info:*' actionformats '[%b|%a]'
+
 # prompt
 function set_prompt(){
-PROMPT="[ ${FC256[033]}%n${COL_DEF}@${FC256[040]}%m${COL_DEF} ${FC256[147]}%d${COL_DEF} ] ${memotxt}
+  PROMPT="[ ${FC256[033]}%n${COL_DEF}@${FC256[040]}%m${COL_DEF} ${FC256[147]}%d${COL_DEF} ] ${vcs_info_msg_0_}${memotxt}
 $ "
 RPROMPT="[Ret:%(?,%{${FC256[195]}%}%?,%{${FC256[196]}%}%?)%{${COL_DEF}%} %*]"
+#RPROMPT=$RPROMPT'${vcs_info_msg_0_}'
 }
 
 
-set_prompt
+precmd () { vcs_info; set_prompt; }
