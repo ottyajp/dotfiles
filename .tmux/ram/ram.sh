8e1499db30f4c8c43fc2684512d05962b18f6355
ottyajp <ottyajp@gmail.com>
2017-09-22 02:18:53 +0900

    小数点以下の数字を削るように

--- .tmux/ram/ram.sh
+++ .tmux/ram/ram.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ram=`free | grep Mem | awk '{print $3/$2*100}'`
-swap=`free | grep Swap | awk '{print $3/$2*100}'`
+ram=`free | grep Mem | awk '{print $3/$2*100}' | sed -e "s/\..\+//"`
+swap=`free | grep Swap | awk '{print $3/$2*100}' | sed -e "s/\..\+//"`
 color='#[fg=colour106]'
 
 echo ${color}RAM:${ram}% SWAP:${swap}%
