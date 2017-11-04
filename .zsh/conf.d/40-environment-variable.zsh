e281caba3b68ee3b18ecbbae6e4d40ad0cda2aac
ottyajp <ottyajp@gmail.com>
2017-11-05 08:26:39 +0900

    fix ruby version

--- .zsh/conf.d/40-environment-variable.zsh
+++ .zsh/conf.d/40-environment-variable.zsh
@@ -3,7 +3,7 @@ export PATH=$PATH:/opt/android-sdk/platform-tools
 export PATH=$PATH:/opt/android-ndk
 export NDKROOT=/opt/android-ndk
 export PATH=$PATH:/home/tea/.cabal/bin
-export PATH=$PATH:/home/tea/.gem/ruby/2.2.0/bin
+export PATH=$PATH:/home/tea/.gem/ruby/2.4.0/bin
 
 export VISUAL="vim"
 export FBFONT=/usr/share/kbd/consolefonts/ter-216n.psf.gz
