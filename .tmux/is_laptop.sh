803266796f29420dd6e12871c026cbfb3d931192
ottyajp <ottyajp@gmail.com>
2017-11-09 18:04:30 +0900

    参考情報を追記

--- .tmux/is_laptop.sh
+++ .tmux/is_laptop.sh
@@ -7,3 +7,28 @@ case $type in
   * ) exit 0 ;;
 esac
 
+# https://superuser.com/questions/877677/programatically-determine-if-an-script-is-being-executed-on-laptop-or-desktop
+# 1 Other
+# 2 Unknown
+# 3 Desktop
+# 4 Low Profile Desktop
+# 5 Pizza Box
+# 6 Mini Tower
+# 7 Tower
+# 8 Portable
+# 9 Laptop
+# 10 Notebook
+# 11 Hand Held
+# 12 Docking Station
+# 13 All in One
+# 14 Sub Notebook
+# 15 Space-Saving
+# 16 Lunch Box
+# 17 Main System Chassis
+# 18 Expansion Chassis
+# 19 SubChassis
+# 20 Bus Expansion Chassis
+# 21 Peripheral Chassis
+# 22 Storage Chassis
+# 23 Rack Mount Chassis
+# 24 Sealed-Case PC
