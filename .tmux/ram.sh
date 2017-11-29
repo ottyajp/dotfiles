49c9c4c131b737d739f5ac95a868551687978378
ottyajp <ottyajp@gmail.com>
2017-11-29 18:39:19 +0900

    /の使用率も表示するように

--- .tmux/ram.sh
+++ .tmux/ram.sh
@@ -3,6 +3,7 @@
 ram=`free | grep Mem | awk '{print $3/$2*100}' | sed -e "s/\..\+//"`
 swap=`free | grep Swap | awk '{print $3/$2*100}' | sed -e "s/\..\+//"`
 tmp=`df -h | grep \/tmp | awk '{print $5}'`
+rootfs=`df -h | grep \/$ | awk '{print $5}'`
 color='#[fg=colour106]'
 
-echo ${color}RAM:${ram}% SWAP:${swap}% /tmp:${tmp}
+echo ${color}RAM:${ram}% SWAP:${swap}% /tmp:${tmp} /:${rootfs}
