ee0b14bdae0f1587a10bb195a9f933fe1097f834
ottyajp <ottyajp@gmail.com>
2017-11-29 18:21:25 +0900

    ステータスバーに/tmpの状況を表示するようにしてみた

--- .tmux/ram.sh
+++ .tmux/ram.sh
@@ -2,6 +2,7 @@
 
 ram=`free | grep Mem | awk '{print $3/$2*100}' | sed -e "s/\..\+//"`
 swap=`free | grep Swap | awk '{print $3/$2*100}' | sed -e "s/\..\+//"`
+tmp=`df -h | grep \/tmp | awk '{print $5}'`
 color='#[fg=colour106]'
 
-echo ${color}RAM:${ram}% SWAP:${swap}%
+echo ${color}RAM:${ram}% SWAP:${swap}% /tmp:${tmp}
