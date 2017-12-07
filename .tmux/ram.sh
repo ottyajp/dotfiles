d42dff9dec5871bebdddb9f2d6eb4321c374813f
ottyajp <ottyajp@gmail.com>
2017-12-07 23:58:44 +0900

    ファイルシステム容量の表示色を変更

--- .tmux/ram.sh
+++ .tmux/ram.sh
@@ -5,5 +5,6 @@ swap=`free | grep Swap | awk '{print $3/$2*100}' | sed -e "s/\..\+//"`
 tmp=`df -h | grep \/tmp | awk '{print $5}'`
 rootfs=`df -h | grep \/$ | awk '{print $5}'`
 color='#[fg=colour106]'
+color2='#[fg=colour053]'
 
-echo ${color}RAM:${ram}% SWAP:${swap}% /tmp:${tmp} /:${rootfs}
+echo ${color}RAM:${ram}% SWAP:${swap}% ${color2}/tmp:${tmp} /:${rootfs}
