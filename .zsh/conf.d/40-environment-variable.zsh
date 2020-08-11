63f54c3b089e7de7c1d8600cbc60620cff54e118
ottyajp <ottyajp@gmail.com>
2020-08-11 18:21:44 +0900

    rubyのパスを環境に沿う感じで

--- .zsh/conf.d/40-environment-variable.zsh
+++ .zsh/conf.d/40-environment-variable.zsh
@@ -3,7 +3,7 @@ export PATH=$PATH:/opt/android-sdk/platform-tools
 export PATH=$PATH:/opt/android-ndk
 export NDKROOT=/opt/android-ndk
 export PATH=$PATH:/home/tea/.cabal/bin
-export PATH=$PATH:/home/tea/.gem/ruby/2.4.0/bin
+export PATH=$(ruby -e 'print Gem.user_dir')/bin:$PATH
 
 export VISUAL="vim"
 export FBFONT=/usr/share/kbd/consolefonts/ter-216n.psf.gz
