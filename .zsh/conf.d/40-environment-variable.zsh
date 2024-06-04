5430dcea8ce00ddbd96fd5a746c8395a08b12ffa
ottyajp <ottyajp@gmail.com>
2024-06-05 02:27:38 +0900

    change on cobaltblue

--- .zsh/conf.d/40-environment-variable.zsh
+++ .zsh/conf.d/40-environment-variable.zsh
@@ -4,6 +4,8 @@ export PATH=$PATH:/opt/android-ndk
 export NDKROOT=/opt/android-ndk
 export PATH=$PATH:/home/tea/.cabal/bin
 export PATH=$(ruby -e 'print Gem.user_dir')/bin:$PATH
+export PATH=$PATH:/home/tea/.gem/ruby/2.4.0/bin
+export PATH=~/bin:$PATH
 
 export VISUAL="vim"
 export FBFONT=/usr/share/kbd/consolefonts/ter-216n.psf.gz
